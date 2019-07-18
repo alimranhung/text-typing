358a043f4a250e7cd956d85c0cec34f70c3f4196
Md Imran Hossain <42710065+alimranhung@users.noreply.github.com>
2019-07-18 20:18:04 +0600

    Add files via upload

---
+++ Text Typing Animation Effects Using Html and CSS - Pure CSS Typing Effect/js/main.js
@@ -0,0 +1,15 @@
+var typed = new Typed('.typing', {
+                strings: [
+                    "First sentence.", 
+                    "Second sentence.",
+                    "thard sentence.", 
+                    "four sentence.",
+                    "five sentence.", 
+                    "six sentence.",
+                    "seven sentence.", 
+                    "eight sentence.",
+                ],
+                typeSpeed: 40,
+                backSpeed: 40,
+                loop:true,
+            });
